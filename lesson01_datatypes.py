a904d822b223605f40f88207bbfc895676ef0233
addanki20292421 <addanki20292421@student.nhvweb.net>
2025-09-24 16:18:09 +0000

    creates data types with strings, integers, floats and booleans

---
+++ lesson01_datatypes.py
@@ -0,0 +1,77 @@
+# this is a comment
+
+
+# 1. strings-words
+print("1. Strings \n\n")
+
+print("Hello World")
+print(" ")
+print("Hello World Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World")
+
+print("1234567890 \n qwerty\nuiop")
+
+print("name: \tbilly \nage: \t14 \nGrade: \t9")
+# integers
+print("2. Integers \n\n")
+print(42)
+
+print(-23)
+
+
+# 67 MANGOS BOIIII TS CODE SO TUFF (you can ignore this mr.harell)
+print(67 + 41)
+
+
+# float decimal
+
+print("3. Float(Decimals) \n\n")
+
+# it may be a normal integer but python stores it as a "Float"
+# ------|
+print(4.0)
+print(2.0+4.5)
+print(2.0 + 4) # returns a float
+
+# BOOLEAN - true or false
+# named after some dude named Bool
+print("4. Booleans \n\n")
+
+print(True) # just a representatin of 1
+print(False) # just a representation of 0
+
+print(True * 67) # this gon be 67
+print(True + True + False) # this is gonna be 2
+
+# check if things are the same or not the same
+# Uses two equals to check if things are equals
+print(1 == 1) # True
+print(1 == 2) # False
+
+
+# not equals is just double equals evil twin
+# with exclaimation point and equal
+print(1 != 1) # False
+print(1 != 2) # True
+
+
+# you can also do it with strings
+
+print("Hello" == "Helo") # false
+
+# its case sensitive
+print("Hello" == "hello") # false
+
+# can use math too
+print(5 + 5 == 11) # False
+
+
+# Data Type lookup
+
+print("5. Data Type Lookup\n\n")
+
+# Python auto detects the data type
+# you can use "type(<the thing>)" to find the type of something(why would anyone need this)
+print(type("this is a string")) # "<class 'str'>"
+print(type(4)) # "<class 'int'>"
+print(type(4.0)) # "<class 'float'>"
+print(type(4 == 4.1)) # "<class 'bool'>"
