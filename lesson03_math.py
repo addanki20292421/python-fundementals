ea531ac4bef4570ea09669edf5e82767c97f5bf6
addanki20292421 <addanki20292421@student.nhvweb.net>
2025-09-26 18:00:32 +0000

    math

---
+++ lesson03_math.py
@@ -0,0 +1,22 @@
+add = 7 + 2
+print("Add: \t",add)
+
+subtract = 7 - 2
+print("Difference: \t",add)
+
+multiply = 7 * 2
+print("Product: \t",multiply)
+
+float_divide = 7 / 2
+print("Float Divide: \t",float_divide)
+
+floor_divide = 7 // 2
+print("Floor Divide: \t",floor_divide)
+
+modulus = 7 % 2
+print("modulus: \t",modulus)
+
+exponent = 7 ** 2
+print("exponenet: \t",exponent)
+
+print((2+3) *4)
