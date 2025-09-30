1ae551280f6ddeaebaa738fc54493fdb767cd41c
addanki20292421 <addanki20292421@student.nhvweb.net>
2025-09-30 15:53:59 +0000

    libraries

---
+++ lesson04_libraries.py
@@ -0,0 +1,11 @@
+#  librarys
+
+import math
+
+print("\n-------------------MATH-------------------\n")
+
+print("square root: \t", math.sqrt(25))
+print("round up: \t", math.ceil(4.2))
+print("round down: \t", math.floor(4.8))
+print("power: \t", math.pow(2, 5))
+print("pi: \t", math.pi)
